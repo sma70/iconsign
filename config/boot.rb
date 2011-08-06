1b25c6a45ee8e7ba592f946d1422acdf5576725a
Shinsuke Murata <shinsuke_murata@hotmail.com>
2011-08-06 00:25:43 -0700

    modified config/boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-require 'rubygems'
+
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
@@ -7,11 +7,3 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 
 
-module Rails
-  class Server
-    alias :default_options_alias :default_options
-    def default_options
-      default_options_alias.merge!(:Port => 3333)
-    end    
-  end
-end
